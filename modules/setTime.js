261673362c1cd03c1e5eb986ceec93f927cdb3ba
Zuhra Hashimi <zuhra.tactaz@yahoo.com>
2022-11-02 22:09:02 +0430

    import all the modules into index.js file

---
+++ modules/setTime.js
@@ -0,0 +1,5 @@
+import Books from './modules/Books.js';
+import snaker from './modules/flashMessage.js';
+import showBlock from './modules/navbar.js';
+import removeBk from './modules/removeBook.js';
+import startTime from './modules/setTime.js';
